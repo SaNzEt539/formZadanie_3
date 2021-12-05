b4a9df9e4303d2901a7a2aa778711d7ffa7c4166
SaNzEt539 <95311995+SaNzEt539@users.noreply.github.com>
2021-12-05 15:47:28 +0300

    Add files via upload

---
+++ fZad1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guiTask_3
+{
+    public partial class fZad1 : Form
+    {
+        static double f(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        }
+        static double d(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+        double x1, x2, y1, y2, a, b, c;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Double.Parse(tbA.Text);
+                b = Double.Parse(tbB.Text);
+                c = Double.Parse(tbC.Text);
+                labRes2.Text = "Периметр: " + (a + b + c).ToString();
+            }
+            catch (System.FormatException)
+            {
+                labRes2.Text = "Введено что-то не то";
+            }
+            finally
+            {
+                labRes2.Visible = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                x1 = Double.Parse(tbX1.Text);
+                y1 = Double.Parse(tbY1.Text);
+                x2 = Double.Parse(tbX2.Text);
+                y2 = Double.Parse(tbY2.Text);
+                labRes1.Text = "Длина отрезка: " + Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            }
+            catch (System.FormatException)
+            {
+                labRes1.Text = "Введено что-то не то";
+            }
+            finally
+            {
+                labRes1.Visible = true;
+            }
+        }
+
+        public fZad1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ fZad2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guiTask_3
+{
+    public partial class fZad2 : Form
+    {
+        double a, b, h;
+        string str = String.Empty, str2 = String.Empty;
+        public fZad2()
+        {
+            InitializeComponent();
+        }
+        private void fZad2_Load(object sender, EventArgs e)
+        {
+
+        }
+        void Func(double a, double b, double h)
+        {
+            for (double x = a; x <= b; x += h)
+            {
+                if ((x - 1) < 1)
+                {
+                    str += "1 ";
+                }
+                else if ((x - 1) == 1)
+                {
+                    str += "0 ";
+                }
+                else
+                {
+                    str += "-1 ";
+                }
+            }
+        }
+        static void Func(double x, out double y)
+        {
+            if ((x - 1) < 1)
+            {
+                y = 1;
+            }
+            else if ((x - 1) == 1)
+            {
+                y = 0;
+            }
+            else
+            {
+                y = -1;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Double.Parse(tbA.Text);
+                b = Double.Parse(tbB.Text);
+                h = Double.Parse(tbH.Text);
+                Func(a, b, h);
+                labRes1.Text = str;
+                for (double x = a; x <= b; x += h)
+                {
+                    double y;
+                    Func(x, out y);
+                    str2 += y + " ";
+                }
+                labRes2.Text = str2;
+            }
+            catch (System.FormatException)
+            {
+                labRes1.Text = "Введено что-то не то";
+            }
+            finally
+            {
+                labRes1.Visible = true;
+                labRes2.Visible = true;
+            }
+        }
+    }
+}
